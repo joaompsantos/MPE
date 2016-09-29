1415ab1ce10df7cc66df85b277c027cbba28e1e7
Joao Santos <santos.mp.joao@gmail.com>
2016-09-29 16:51:34 +0100

    Updated class 02

--- 02/pt3.m
+++ 02/pt3.m
@@ -1,10 +1,15 @@
 %% Ex 1
-N=1000; % Execuções
-
-P_A=sum(sum(ceil(6*rand(4,N))==6)>0)/N;
+N=10000; % Execuções
+D1 = ceil(6*rand(4,N));
+condB = (D1==6);
+nsix = sum(condB);
+P_A = sum(nsix>0)/N;
 fprintf('Probabilidade de A: %.3f\n',P_A);
 
 
-
-P_B=sum(sum(ceil((6*rand(24,N))+ceil(6*rand(24,N)))==12)>0)/N;
+D1 = ceil(6*rand(24,N));
+D2 = ceil(6*rand(24,N));
+condB = (D1==6)&(D2==6);
+nduplos = sum(condB);
+P_B = sum(nduplos>=1)/N;
 fprintf('Probabilidade de B: %.3f\n',P_B);
